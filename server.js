782279fe2a8277de77a7569ce78c4841506af82e
Manjunath <madkdsk123@gmail.com>
2024-11-11 17:19:32 +0530

    First git commit of project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+app.get('/', (req, res) => res.send('Welcome to Smart Expense Tracker'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
